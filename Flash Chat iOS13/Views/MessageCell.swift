b697c12ac547ac8e5d1f762afba9500d4d8f9bbf
aarish21 <aarish.jerry@gmail.com>
2021-05-29 02:38:45 +0530

    initial

---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,30 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Aarish Rahman on 24/05/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet var messageBubble: UIView!
+    @IBOutlet var label: UILabel!
+    
+    @IBOutlet var leftImageView: UIImageView!
+    
+    @IBOutlet var rightImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height/5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,21 @@
+
+# platform :ios, '9.0'
+
+target 'Flash Chat iOS13' do
+  pod 'CLTypingLabel'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Firestore'
+  use_frameworks!
+
+  # Pods for Flash Chat iOS13
+
+end
+post_install do |installer|
+   installer.pods_project.targets.each do |target|
+       target.build_configurations.each do |config|
+          if config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'].to_f < 9.0
+            config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '9.0'
+          end
+       end
+   end
+end
